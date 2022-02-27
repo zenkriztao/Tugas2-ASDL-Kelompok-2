f31313c8a6fca80f9a6e1ac178240b5ab1eca56f
Abdullah Farauk <abdullahfarauk@gmail.com>
2022-02-28 01:22:52 +0700

    Merapikan MainDraft.java

--- tugas-2a/MainDraft.java
+++ tugas-2a/MainDraft.java
@@ -242,87 +242,87 @@ public class MainDraft {
                             "Input pilihan : ");
                         pilihanOperasi = input.nextInt();
                         System.out.println( );
-                            switch(pilihanOperasi){
-                                case 1:
-                                    //PENAMBAHAN DATA PUSTAKA
+                        switch(pilihanOperasi){
+                            case 1:
+                                //PENAMBAHAN DATA PUSTAKA
 
-                                break;    
-                                case 2:
-                                    int pilihanPustakaPenghapusan;
-                                    String sign;
-                                    do{
-                                        System.out.println();
-                                        System.out.println("*** OPERASI HAPUS DATA DOUBLE LINKED LIST ***");
-                                        System.out.println("    1. Hapus Data Awal" + 
-                                                "\n    2. Hapus Data Tengah" + 
-                                                "\n    3. Hapus Data Akhir" + 
-                                                "\n    4. Cetak Data" + 
-                                                "\n    5. Kembali");
-                                        System.out.print("    Silahkan Pilih (1/2/3/4/5): ");
-                                        pilihanPustakaPenghapusan = input.nextInt();
-                                        switch(pilihanPustakaPenghapusan){
-                                            case 1:
-                                                System.out.println("\n*** OPERASI HAPUS DATA AWAL DOUBLE LINKED LIST ***");
-                                                System.out.println("\n    INFO: - Masukan Y (ENTER) untuk menghapus data \n\t  - Masukan T untuk kembali ke menu sebelumnya\n");
-                                                System.out.println("    Yakin ingin menghapus data di awal ? Y ");
-                                                System.out.print("    (Masukan Pilihan): ");
-                                                sign = input.next();
-                                                switch(sign){
-                                                    case "Y", "y":
-                                                        link.removeFirst();
-                                                        System.out.println("    Data awal berhasil dihapus.");
-                                                    break;
-                                                }
-                                            break;
-                                            case 2:
-                                                System.out.println("\n*** OPERASI HAPUS DATA TENGAH DOUBLE LINKED LIST ***");
-                                                System.out.println("\n    INFO: - Masukan Y (ENTER) untuk menghapus data \n\t  - Masukan T untuk kembali ke menu sebelumnya\n");
-                                                System.out.println("    Yakin ingin menghapus data di tengah ? Y ");
-                                                System.out.print("    (Masukan Pilihan): ");
-                                                sign = input.next();
-                                                switch(sign){
-                                                    case "Y", "y":
-                                                        link.remove(link.size() / 2);
-                                                        System.out.println("    Data tengah berhasil dihapus.");
-                                                    break;
-                                                }
-                                            break;
-                                            case 3:
-                                                System.out.println("\n*** OPERASI HAPUS DATA AKHIR DOUBLE LINKED LIST ***");
-                                                System.out.println("\n    INFO: - Masukan Y (ENTER) untuk menghapus data \n\t  - Masukan T untuk kembali ke menu sebelumnya\n");
-                                                System.out.println("    Yakin ingin menghapus data di akhir ? Y ");
-                                                System.out.print("    (Masukan Pilihan): "); 
-                                                sign = input.next();
-                                                switch(sign){
-                                                    case "Y", "y":
-                                                        link.removeLast();
-                                                        System.out.println("    Data akhir berhasil dihapus.");
-                                                    break;
-                                                }
-                                            break;
-                                            case 4:
-                                                System.out.println("\n*** CETAK DATA DOUBLE LINKED LIST ***\n");
-                                                System.out.println("    " + link);
-                                                System.out.println("\n    INFO: - Masukan 5 untuk kembali ke menu sebelumnya");
-                                                System.out.print("    ");
-                                                input.nextInt();
-                                            break;
-                                            case 5:
-                                                System.out.println();
-                                            break;
-                                            default:
-                                                System.out.println("    Pilihan tidak valid."); 
-                                        }
-                                    }while(pilihanPustakaPenghapusan != 5);
-                                break;
-                                case 3:
-                                //PENCARIAN/PENGUBAHAN DATA
+                            break;    
+                            case 2:
+                                int pilihanPustakaPenghapusan;
+                                String sign;
+                                do{
+                                    System.out.println();
+                                    System.out.println("*** OPERASI HAPUS DATA DOUBLE LINKED LIST ***");
+                                    System.out.println("    1. Hapus Data Awal" + 
+                                            "\n    2. Hapus Data Tengah" + 
+                                            "\n    3. Hapus Data Akhir" + 
+                                            "\n    4. Cetak Data" + 
+                                            "\n    5. Kembali");
+                                    System.out.print("    Silahkan Pilih (1/2/3/4/5): ");
+                                    pilihanPustakaPenghapusan = input.nextInt();
+                                    switch(pilihanPustakaPenghapusan){
+                                        case 1:
+                                            System.out.println("\n*** OPERASI HAPUS DATA AWAL DOUBLE LINKED LIST ***");
+                                            System.out.println("\n    INFO: - Masukan Y (ENTER) untuk menghapus data \n\t  - Masukan T untuk kembali ke menu sebelumnya\n");
+                                            System.out.println("    Yakin ingin menghapus data di awal ? Y ");
+                                            System.out.print("    (Masukan Pilihan): ");
+                                            sign = input.next();
+                                            switch(sign){
+                                                case "Y", "y":
+                                                    link.removeFirst();
+                                                    System.out.println("    Data awal berhasil dihapus.");
+                                                break;
+                                            }
+                                        break;
+                                        case 2:
+                                            System.out.println("\n*** OPERASI HAPUS DATA TENGAH DOUBLE LINKED LIST ***");
+                                            System.out.println("\n    INFO: - Masukan Y (ENTER) untuk menghapus data \n\t  - Masukan T untuk kembali ke menu sebelumnya\n");
+                                            System.out.println("    Yakin ingin menghapus data di tengah ? Y ");
+                                            System.out.print("    (Masukan Pilihan): ");
+                                            sign = input.next();
+                                            switch(sign){
+                                                case "Y", "y":
+                                                    link.remove(link.size() / 2);
+                                                    System.out.println("    Data tengah berhasil dihapus.");
+                                                break;
+                                            }
+                                        break;
+                                        case 3:
+                                            System.out.println("\n*** OPERASI HAPUS DATA AKHIR DOUBLE LINKED LIST ***");
+                                            System.out.println("\n    INFO: - Masukan Y (ENTER) untuk menghapus data \n\t  - Masukan T untuk kembali ke menu sebelumnya\n");
+                                            System.out.println("    Yakin ingin menghapus data di akhir ? Y ");
+                                            System.out.print("    (Masukan Pilihan): "); 
+                                            sign = input.next();
+                                            switch(sign){
+                                                case "Y", "y":
+                                                    link.removeLast();
+                                                    System.out.println("    Data akhir berhasil dihapus.");
+                                                break;
+                                            }
+                                        break;
+                                        case 4:
+                                            System.out.println("\n*** CETAK DATA DOUBLE LINKED LIST ***\n");
+                                            System.out.println("    " + link);
+                                            System.out.println("\n    INFO: - Masukan 5 untuk kembali ke menu sebelumnya");
+                                            System.out.print("    ");
+                                            input.nextInt();
+                                        break;
+                                        case 5:
+                                            System.out.println();
+                                        break;
+                                        default:
+                                            System.out.println("    Pilihan tidak valid."); 
+                                    }
+                                }while(pilihanPustakaPenghapusan != 5);
+                            break;
+                            case 3:
+                            //PENCARIAN/PENGUBAHAN DATA
 
-                                case 4:
-                                break;
-                                default:
+                            case 4:
+                            break;
+                            default:
                                     System.out.println("    Pilihan tidak valid.");
-                            }       
+                        }       
                     }while(pilihanOperasi != 4);
                 break;
 
