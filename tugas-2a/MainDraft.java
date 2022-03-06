ecb8018eab34545177c4c0c237bf1989bd1dde5c
Kaka Inochi <100188519+s3nyu@users.noreply.github.com>
2022-03-06 14:42:09 +0700

    Update MainDraft.java

--- tugas-2a/MainDraft.java
+++ tugas-2a/MainDraft.java
@@ -621,7 +621,60 @@ public class MainDraft {
                         switch(pilihanOperasi){
                             case 1:
                                 //PENAMBAHAN DATA PUSTAKA
+                                int pilihanPenambahanData;
 
+                                System.out.println("\n*** OPERASI TAMBAH DATA DOUBLE LINKED LIST ***");
+                                System.out.println("1. Tambah Data Awal");
+                                System.out.println("2. Tambah Data Tengah");
+                                System.out.println("3. Tambah Data Akhir");
+                                System.out.println("4. Cetak Data");
+                                System.out.println("5. Kembali");
+                    
+                                System.out.print("Silahkan pilih [1/2/3/4/5] : ");
+
+                                pilihanPenambahanData = input.nextInt();
+
+                                switch(pilihanPenambahanData){
+                                    case 1:
+                                    System.out.println(link);
+                                    System.out.print("Tambah data awal : ");
+                                    int awal = input.nextInt();
+                                    link.addFirst(awal);
+                                    System.out.println(link);
+                                    break;
+
+                                    case 2:
+                                    System.out.println("*** OPERASI TAMBAH DATA TENGAH DOUBLE LINKED LIST ***");
+                                    System.out.println(link);
+                                    System.out.print("Tambah data tengah : ");
+                                    int tengah = input.nextInt();
+                                    link.add(2, tengah);
+                                    System.out.println(link);
+                                    break;
+
+                                    case 3:
+                                    System.out.println(link);
+                                    System.out.print("Tambah data akhir : ");
+                                    int akhir = input.nextInt();
+                                    link.addLast(akhir);
+                                    System.out.println(link);
+                                    break;
+
+                                    case 4:
+                                    System.out.println("*** DATA LINKED LIST***");
+                                    System.out.println(link);
+                                    break;
+
+                                    case 5:
+                                    System.out.println("    " + link);
+                                    System.out.println("\n    INFO: - Masukan 5 untuk kembali ke menu sebelumnya");
+                                    System.out.print("    ");
+                                    input.nextInt(); input.nextLine();
+                                }
+
+                                  
+
+        System.out.print("Silahkan pilih [1/2/3/4] : ");
                                 break;    
                             case 2:
                                 int pilihanPustakaPenghapusan;
@@ -714,5 +767,3 @@ public class MainDraft {
         input.close();
     }
 }
-
-
